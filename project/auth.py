79f1d96c0e8ca5b06f1eeceb176524e7f544ed4d
Ali <alidevecioglu12324@gmail.com>
2022-12-24 17:11:26 +0200

    Auth Update

--- project/auth.py
+++ project/auth.py
@@ -1,7 +1,7 @@
 # auth.py
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 from Crypto.Cipher import DES
 from flask_login import login_user, logout_user, login_required
 from .models import User
